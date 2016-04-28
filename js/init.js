c05ffd328b6557f22cf5685b578206022e67676f
denFete <halling.7@hotmail.com>
2016-04-29 01:02:05 +0200

    Animations

--- js/init.js
+++ js/init.js
@@ -1,8 +1,25 @@
+    $(window).resize(function(){
+	    windowHeight = $(window).height(); // get new height after change
+	    $('#index-banner').height(windowHeight);
+	});
+	$('#index-banner').css('height', $(window).height());
+
+	 $(".scroll").click(function(event) {
+	    event.preventDefault();
+	    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000, "swing");
+	    } );
+
 (function($){
   $(function(){
 
+
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+
+	
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
