bdf1fec65d988eaa1331272dadb6029c14947643
denFete <halling.7@hotmail.com>
2016-04-29 01:03:59 +0200

    Test

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@
 
 	 $(".scroll").click(function(event) {
 	    event.preventDefault();
-	    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000, "swing");
+	    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1001, "swing");
 	    } );
 
 (function($){
